95bc730f87f0451c283ac684cc8df4267b36d33e
AntorAhammed <antorahmed1616@gmail.com>
2022-03-02 19:11:21 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
+import Slider from './Components/Slider/Slider';
+
+function App() {
+  return (
+    <div>
+        <Navbar/>
+        <Banner/>
+        <Slider/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
